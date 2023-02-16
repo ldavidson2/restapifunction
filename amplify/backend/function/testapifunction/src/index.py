154388b131c275311a427aeedc6a617b6d4d363d
Leo Davidson <94934268+ldavidson2@users.noreply.github.com>
2023-02-15 20:02:55 -0600

    edited api

--- amplify/backend/function/testapifunction/src/index.py
+++ amplify/backend/function/testapifunction/src/index.py
@@ -4,7 +4,7 @@ import boto3
 dynamodb = boto3.resource('dynamodb', region_name="us-east-2",
          aws_access_key_id="AKIAX3FHVXYSKFFYPGE7",
          aws_secret_access_key= "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")
-table = dynamodb.Table('RestApiDB-staging')
+table = dynamodb.Table('resttest-staging')
 
 def handler(event, context):
     response = table.get_item(
---
+++ src/components/message.js
@@ -0,0 +1,33 @@
+import React from "react";
+import { useState, useEffect } from "react";
+import awsmobile from "../aws-exports";
+import { Amplify, API } from "aws-amplify";
+
+Amplify.configure(awsmobile);
+API.configure(awsmobile);
+
+const myAPI = "testapi";
+const path = "/items";
+
+export default function Message() {
+  const [result, setresult] = useState(null);
+  const message = async () => {
+    try {
+      let res = await API.get(myAPI, path, {});
+      let result = res.data;
+      setresult(result);
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
+  useEffect(() => {
+    message();
+  }, []);
+  return (
+    <div>
+      <h1>Results:</h1>
+      {result}
+    </div>
+  );
+}
