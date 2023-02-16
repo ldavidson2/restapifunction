ff680721f537377d42aa26d28420694e8d9eb933
Leo Davidson <94934268+ldavidson2@users.noreply.github.com>
2023-02-16 08:34:40 -0600

    changed api function

--- amplify/backend/function/testapifunction/src/index.py
+++ amplify/backend/function/testapifunction/src/index.py
@@ -1,7 +1,18 @@
 import json
+import boto3
+
+dynamodb = boto3.resource('dynamodb', region_name="us-east-2",
+         aws_access_key_id="AKIAX3FHVXYSKFFYPGE7",
+         aws_secret_access_key= "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")
+table = dynamodb.Table('resttest-staging')
 
 def handler(event, context):
-  print('received event:')
-  print(event)
+    response = table.query(
+    KeyConditionExpression=Key('PK').eq('COMP#0')
+    )
+
+    data = str(response['Items'])
+    print('received event:')
+    print(event)
   
-  return str(event)
+    return data
