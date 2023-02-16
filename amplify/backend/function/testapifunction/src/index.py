665e07033c75cd6879466bb646039a660225f835
Leo Davidson <94934268+ldavidson2@users.noreply.github.com>
2023-02-16 07:54:59 -0600

    changed api

--- amplify/backend/function/testapifunction/src/index.py
+++ amplify/backend/function/testapifunction/src/index.py
@@ -1,21 +1,15 @@
 import json
-import boto3
-
-dynamodb = boto3.resource('dynamodb', region_name="us-east-2",
-         aws_access_key_id="AKIAX3FHVXYSKFFYPGE7",
-         aws_secret_access_key= "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")
-table = dynamodb.Table('resttest-staging')
 
 def handler(event, context):
-    response = table.get_item(
-        Key={
-            'PK': 'COMP#0',
-            'SK': 'COMP#0'
-        }
-    )
-
-    data = str(response)
   print('received event:')
   print(event)
   
-  return data
+  return {
+      'statusCode': 200,
+      'headers': {
+          'Access-Control-Allow-Headers': '*',
+          'Access-Control-Allow-Origin': '*',
+          'Access-Control-Allow-Methods': 'OPTIONS,POST,GET'
+      },
+      'body': json.dumps('Hello from your new Amplify Python lambda!')
+  }
